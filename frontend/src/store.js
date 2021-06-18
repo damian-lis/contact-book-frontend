96c35b457448d397eae5c2d003fa49028700dae0
Damian Lis <foxprogramming93@gmail.com>
2021-06-18 16:21:53 +0200

    Created contact reducer with constants

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { contactReducer } from 'reducers/contact.reducers';
 
-const reducers = combineReducers({});
+const reducers = combineReducers({
+  contacts: contactReducer
+});
 
 const initialState = {};
 
