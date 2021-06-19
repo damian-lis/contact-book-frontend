23c2412b3c5e8779bb8a219b6df48c4feb685f0a
Damian Lis <foxprogramming93@gmail.com>
2021-06-19 15:33:23 +0200

    Enabled to fetch data to redux via the Contact page

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Header from 'components/Header';
 import ContactForm from 'components/ContactForm';
 import ContactTable from 'components/ContactTable';
+import { fetchContacts } from 'actions/contact.actions';
 
 const Index = () => {
   const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
 
   const handleOpen = () => {
     setOpen(true);
@@ -13,6 +16,10 @@ const Index = () => {
   const handleClose = () => {
     setOpen(false);
   };
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <div>
