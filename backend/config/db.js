2056bc3c0c8809c3f7940367d83d54bbc9d866e3
Damian Lis <foxprogramming93@gmail.com>
2021-06-19 13:28:06 +0200

    Used environment variables in server.js and db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'
+import { MONGO_URI } from '../../env.js'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/contacts', {
+    const conn = await mongoose.connect(process.env.MONGO_URI || MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
+import { PORT } from '../env.js'
 import express from 'express'
 import connectDB from './config/db.js'
 
 connectDB()
 
 const app = express()
-const PORT = 5000
 
-app.listen(PORT, console.log(`Server is running on ${PORT}`))
+app.listen(
+  process.env.PORT || PORT,
+  console.log(`Server is running on ${PORT}`)
+)
