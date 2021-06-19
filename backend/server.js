68825878ba86167d3f152ebfcfc1dd36f4ba8587
Damian Lis <foxprogramming93@gmail.com>
2021-06-19 13:35:55 +0200

    Used contactRoutes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import { PORT } from '../env.js'
 import express from 'express'
 import connectDB from './config/db.js'
+import contactRoutes from './routes/contact.routes.js'
 
 connectDB()
 
 const app = express()
+
+app.use('/contact', contactRoutes)
 
 app.listen(
   process.env.PORT || PORT,
