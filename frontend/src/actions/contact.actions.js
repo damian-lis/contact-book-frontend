deec1fa4d3a2fa75cb41dbe65e9acf5baabf9c69
Damian Lis <foxprogramming93@gmail.com>
2021-06-19 18:14:47 +0200

    Slightly modified current frontend contact actions

--- frontend/src/actions/contact.actions.js
+++ frontend/src/actions/contact.actions.js
@@ -1,39 +1,19 @@
 import axios from 'axios';
 
-import { CONTACT_CREATE, CONTACT_FETCH_ALL } from 'constants/contact.constants';
+import { CONTACT_CREATE, CONTACT_FETCH_ALL, CONTACT_DELETE } from 'constants/contact.constants';
 
-export const createContact = (form) => async (dispatch, getState) => {
+export const createContact = (form) => async (dispatch) => {
   try {
-    const {
-      userLogin: { userInfo }
-    } = getState();
-
-    const config = {
-      headers: {
-        Authorization: `Bearer ${userInfo.token}`
-      }
-    };
-
-    const { data } = await axios.post('/contact', form, config);
+    const { data } = await axios.post('/contact', form);
     dispatch({ type: CONTACT_CREATE, payload: data });
   } catch (error) {
     console.log(error);
   }
 };
 
-export const fetchContacts = () => async (dispatch, getState) => {
+export const fetchContacts = () => async (dispatch) => {
   try {
-    const {
-      userLogin: { userInfo }
-    } = getState();
-
-    const config = {
-      headers: {
-        Authorization: `Bearer ${userInfo.token}`
-      }
-    };
-
-    const { data } = await axios.get('/contact', config);
+    const { data } = await axios.get('/contact');
     dispatch({ type: CONTACT_FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error);
