612660a35a5d652c4b93b49be075d4e4d0635742
Damian Lis <foxprogramming93@gmail.com>
2021-06-20 19:11:22 +0200

    Added an authorization header containing a user token to each contact action

--- frontend/src/actions/contact.actions.js
+++ frontend/src/actions/contact.actions.js
@@ -8,46 +8,100 @@ import {
   CONTACT_UPDATE
 } from 'constants/contact.constants';
 
-export const createContact = (form) => async (dispatch) => {
+export const createContact = (form) => async (dispatch, getState) => {
   try {
-    const { data } = await axios.post('/contact', form);
+    const {
+      userLogin: { userInfo }
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    };
+
+    const { data } = await axios.post('/contact', form, config);
+
     dispatch({ type: CONTACT_CREATE, payload: data });
   } catch (error) {
     console.log(error);
   }
 };
 
-export const fetchContacts = () => async (dispatch) => {
+export const fetchContacts = () => async (dispatch, getState) => {
   try {
-    const { data } = await axios.get('/contact');
+    const {
+      userLogin: { userInfo }
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    };
+
+    const { data } = await axios.get('/contact', config);
+
     dispatch({ type: CONTACT_FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error);
   }
 };
 
-export const deleteContact = (id) => async (dispatch) => {
+export const deleteContact = (id) => async (dispatch, getState) => {
   try {
-    await axios.delete(`/contact/${id}`);
+    const {
+      userLogin: { userInfo }
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    };
+
+    await axios.delete(`/contact/${id}`, config);
+
     dispatch({ type: CONTACT_DELETE, payload: id });
   } catch (error) {
     console.log(error);
   }
 };
 
-export const deleteContacts = (ids) => async (dispatch) => {
+export const deleteContacts = (ids) => async (dispatch, getState) => {
   try {
-    await axios.post(`/contact/delete`, ids);
+    const {
+      userLogin: { userInfo }
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    };
+
+    await axios.post(`/contact/delete`, ids, config);
+
     dispatch({ type: CONTACT_DELETE_ALL, payload: ids });
   } catch (error) {
     console.log(error);
   }
 };
 
-export const updateContact = (id, form) => async (dispatch) => {
-  console.log(id);
+export const updateContact = (id, form) => async (dispatch, getState) => {
   try {
-    const { data } = await axios.put(`/contact/${id}`, form);
+    const {
+      userLogin: { userInfo }
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    };
+
+    const { data } = await axios.put(`/contact/${id}`, form, config);
+
     console.log(data);
     dispatch({ type: CONTACT_UPDATE, payload: data });
   } catch (error) {
