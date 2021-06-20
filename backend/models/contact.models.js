972846e9f81bc8d5be49b1bb935780f2f408b2da
Damian Lis <foxprogramming93@gmail.com>
2021-06-20 19:02:09 +0200

    Added user to contactSchema

--- backend/models/contact.models.js
+++ backend/models/contact.models.js
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose'
 
 const contactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
   name: String,
   email: String,
   phoneNo1: String,
