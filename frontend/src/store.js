666de6265a4d4065c6e7c20f8f95e3c26eb0d6dc
Damian Lis <foxprogramming93@gmail.com>
2021-06-20 15:43:00 +0200

    Added authReducer and set initial state in store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,12 +2,18 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { contactReducer } from 'reducers/contact.reducers';
+import { authReducer } from 'reducers/auth.reducers';
 
 const reducers = combineReducers({
-  contacts: contactReducer
+  contacts: contactReducer,
+  userLogin: authReducer
 });
 
-const initialState = {};
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
+const initialState = { userLogin: { userInfo: userInfoFromStorage } };
 
 const middleware = [thunk];
 
