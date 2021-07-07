6b7551b36cb2c79cd577f91c3a969bc22e7bd25e
Damian Lis <foxprogramming93@gmail.com>
2021-07-07 14:13:00 +0200

    Moved the backend logic to a separate contact-book-backend app

--- backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const router = express.Router()
-
-import { loginUser, registerUser } from '../controllers/auth.controllers.js'
-
-router.route('/login').post(loginUser)
-router.route('/').post(registerUser)
-
-export default router
--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, 'DamianLis@2021', {
-    expiresIn: '30d',
-  })
-}
-
-export default generateToken
