3dd3b975aeb7e66eccf2e759beffdc40c9c4b686
Damian Lis <foxprogramming93@gmail.com>
2021-07-11 11:11:02 +0200

    Normalized the styles

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 ReactDOM.render(
   <Provider store={store}>
+    <CssBaseline />
     <App />
   </Provider>,
   document.getElementById('root')
